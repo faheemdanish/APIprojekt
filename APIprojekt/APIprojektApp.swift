9e2b9243d1cd7fe922a79c1837962df19da1cffa
Faheem Danish <danish42005@gmail.com>
2023-11-29 15:38:20 +0100

    Initial Commit

---
+++ APIprojekt/APIprojektApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIprojektApp.swift
+//  APIprojekt
+//
+//  Created by Faheem Danish on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct APIprojektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIprojekt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APIprojekt
+//
+//  Created by Faheem Danish on 2023-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
